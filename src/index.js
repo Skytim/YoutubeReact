ccbb85f5dc84ea7d6d093da9448d1d42a41be883
蔡宇祥 <sky22357168@gmail.com>
2017-03-20 23:29:36 +0800

    Ｄetail Component and template strings

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar';
-
-
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyA0D_h0ouhGomC9xUTZHG_1uZ3X6aFdao0';
 
 
+
 // Create a new Component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+            this.setState({ videos: data });
+        });
+    }
+    render() {
+        
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 //Take this component's generated HTML and put it
 // on the page (in the DOM)

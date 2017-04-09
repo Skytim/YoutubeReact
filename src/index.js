1d1a1774bd1da4edce81a465925005a2ecf56570
蔡宇祥 <sky22357168@gmail.com>
2017-04-09 14:37:51 +0800

    Video Selected

--- src/index.js
+++ src/index.js
@@ -12,19 +12,27 @@ const API_KEY = 'AIzaSyA0D_h0ouhGomC9xUTZHG_1uZ3X6aFdao0';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
-            this.setState({ videos: data });
+        YTSearch({ key: API_KEY, term: 'microsoft' }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
     render() {
-        
+
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
             </div>
         );
     }
